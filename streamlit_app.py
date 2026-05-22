f4b2c8ea402f944b48f0fbd6facf44adb515b829
Dhanshree Randhavane <randhavanedhanshree@gmail.com>
2026-05-22 14:42:12 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,57 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# App title
+st.title("🥤 Customize Your Smoothie! 🥤")
+
+st.write(
+    """Choose the fruits you want in your custom Smoothie!"""
+)
+
+# Name input
+name_on_order = st.text_input('Name on Smoothie:')
+
+st.write('The name on your Smoothie will be:', name_on_order)
+
+# Snowflake session
+session = get_active_session()
+
+# Read fruit table
+my_dataframe = session.table(
+    "smoothies.public.fruit_options"
+).select(col('FRUIT_NAME'))
+
+# Fruit selection
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
+)
+
+# If fruits selected
+if ingredients_list:
+
+    ingredients_string = ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    # Create insert statement
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
+    # Show SQL query
+    st.write(my_insert_stmt)
+
+    # Stop app for debugging
+    #st.stop()
+
+    # Submit button
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+
+        st.success('Your Smoothie is ordered!', icon="✅")
