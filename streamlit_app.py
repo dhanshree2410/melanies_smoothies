45d9e72a3cf4b57d3b0adcbdd0afdba81f6798de
Dhanshree Randhavane <randhavanedhanshree@gmail.com>
2026-05-22 17:07:36 +0530

    Refactor Snowflake connection and order submission
    Fix connection handling and update SQL insert statement.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,15 @@
 import streamlit as st
 
-# Snowflake connection
 cnx = st.connection("snowflake")
 
-# App title
 st.title("🥤 Customize Your Smoothie! 🥤")
 
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-# Name input
 name_on_order = st.text_input("Name on Smoothie:")
 
 st.write("The name on your Smoothie will be:", name_on_order)
 
-# Read fruits from Snowflake
 fruit_df = cnx.query("""
     SELECT FRUIT_NAME
     FROM SMOOTHIES.PUBLIC.FRUIT_OPTIONS
@@ -21,14 +17,12 @@ fruit_df = cnx.query("""
 
 fruit_list = fruit_df["FRUIT_NAME"].tolist()
 
-# Fruit selection
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     fruit_list,
     max_selections=5
 )
 
-# Submit order
 if ingredients_list:
 
     ingredients_string = " ".join(ingredients_list)
@@ -42,7 +36,7 @@ if ingredients_list:
         ('{ingredients_string}', '{name_on_order}')
         """
 
-        conn = cnx.raw_connection()
+        conn = cnx.raw_connection
         cursor = conn.cursor()
 
         try:
@@ -52,6 +46,5 @@ if ingredients_list:
 
         finally:
             cursor.close()
-            conn.close()
 
       
